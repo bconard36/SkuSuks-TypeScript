34ae80a93c0d2ff400c5d601af8983db93f850b8
Billy Conard <billy.conard@pcc.edu>
2026-08-28 16:19:32 -0700

    refactor: removed the dead social links from the footer and refactored layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,52 +6,37 @@ import { Link } from "react-router";
 // TODO: Remove dead links from footer
 const Footer = () => {
   return (
-    <footer>
-      <h2>Like, Follow, &amp; Share!</h2>
-      <div id="socials">
-        <Link to="">
-          <svg
-            width="100px"
-            height="100px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            id="facebook-icon"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M23 12.0672C23 5.95496 18.0751 1 12 1C5.92486 1 1 5.95496 1 12.0672C1 17.5912 5.02254 22.1697 10.2812 
-            23V15.2663H7.48828V12.0672H10.2812V9.62898C10.2812 6.85525 11.9235 5.32313 14.4361 5.32313C15.6396 5.32313 16.8984 5.53929 16.8984 5.53929V8.26287H15.5114C14.1449 
-            8.26287 13.7188 9.11597 13.7188 9.99119V12.0672H16.7695L16.2818 15.2663H13.7188V23C18.9775 22.1697 23 17.5912 23 12.0672Z"
-              fill=""
-            />
-          </svg>
-        </Link>
-        <Link to="https://www.instagram.com/skusuks/" target="_blank">
-          <svg
-            width="100px"
-            height="100px"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            id="instagram-icon"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 
+    <>
+      <footer>
+        <div className="footer-contents">
+          <div className="footer-social-container">
+            <h2>Follow Along</h2>
+            <p className="cta">New pieces, updates, and drops on Instagram</p>
+            <div className="icon-handle-container">
+              <Link to="https://www.instagram.com/skusuks/" target="_blank">
+                <svg
+                  width="100px"
+                  height="100px"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  id="instagram-icon"
+                >
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 
             18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
-              fill=""
-            />
-            <path
-              d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
-              fill=""
-            />
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 
+                    fill=""
+                  />
+                  <path
+                    d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"
+                    fill=""
+                  />
+                  <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 
             21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 
             16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 
             4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 
@@ -60,29 +45,24 @@ const Footer = () => {
             20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 
             19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 
             5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"
-              fill=""
-            />
-          </svg>
-        </Link>
-        <Link to="">
-          <svg
-            width="100px"
-            height="100px"
-            viewBox="0 0 1200 1227"
-            fill=""
-            xmlns="http://www.w3.org/2000/svg"
-            id="x-icon"
-          >
-            <path
-              d="M714.163 519.284L1160.89 0H1055.03L667.137 450.887L357.328 0H0L468.492 681.821L0 1226.37H105.866L515.491 750.218L842.672 1226.37H1200L714.137 
-            519.284H714.163ZM569.165 687.828L521.697 619.934L144.011 79.6944H306.615L611.412 515.685L658.88 583.579L1055.08 1150.3H892.476L569.165 687.854V687.828Z"
-              fill=""
-            />
-          </svg>
-        </Link>
-      </div>
-      <p>&copy; 2026 SkuSuks. All rights reserved.</p>
-    </footer>
+                    fill=""
+                  />
+                </svg>
+              </Link>
+              <p id="handle">@skusuks</p>
+            </div>
+          </div>
+          <div className="footer-brand">
+            <img
+              src="../../Images/cropped-inverted-skusuks-logo.png"
+              className="footer-brand-image"
+              alt="SkuSuks brand logo"
+            ></img>
+          </div>
+        </div>
+        <p>&copy; 2026 SkuSuks. All rights reserved.</p>
+      </footer>
+    </>
   );
 };
 
