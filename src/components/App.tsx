52d291a929dd58e9f6c2c867e7e502f9eed470a2
Billy Conard <billy.conard@pcc.edu>
2026-08-31 09:05:43 -0700

    refactor: commented out imports for new drop and pre order components. dead routes with no meaningful content to include before shopify integration.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,8 +3,8 @@ import NavMenu from "./NavMenu";
 import Home from "./Home";
 import Apparel from "./Apparel";
 import Accessories from "./Accessories";
-import PreOrder from "./PreOrder";
-import NewDrops from "./NewDrops";
+// import PreOrder from "./PreOrder";
+// import NewDrops from "./NewDrops";
 import Contact from "./Contact";
 import Donate from "./Donate";
 import Cart from "./Cart";
@@ -22,8 +22,8 @@ function App() {
               <Route path="/" element={<Home />} />
               <Route path="/apparel" element={<Apparel />} />
               <Route path="/accessories" element={<Accessories />} />
-              <Route path="/preorder" element={<PreOrder />} />
-              <Route path="/newdrops" element={<NewDrops />} />
+              {/* <Route path="/preorder" element={<PreOrder />} /> */}
+              {/* <Route path="/newdrops" element={<NewDrops />} /> */}
               <Route path="/contact" element={<Contact />} />
               <Route path="/donate" element={<Donate />} />
               <Route path="/cart" element={<Cart />} />
